70589903683bef47945afb9219642c7c162f9367
zhangchi <imzc.2004@gmail.com>
2025-12-28 23:25:43 +0800

    1

---
+++ vllm1.sh
@@ -0,0 +1,67 @@
+#
+input_length=500
+num_prompts=25
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+input_length_upper=$((input_length + 100))
+repeat_count=$((5000 / num_prompts))
+python benchmarks/benchmark_prefix_caching.py \
+    --model ibm-granite/granite-4.0-h-small \
+    --num-prompts $num_prompts \
+    --repeat-count $repeat_count \
+    --input-length-range $input_length:$input_length_upper \
+    --tensor-parallel-size 4 \
+    --dtype bfloat16 \
+    --enable-prefix-caching
+
+ # For num
+# git switch zc-1
+# for num_prompts in 1 5 10 25 50 100 500; do
+#     for input_length in 0 100 200 400 800 1600; do
+#         export CUDA_VISIBLE_DEVICES=0,1,2,3
+#         input_length_upper=$((input_length + 100))
+#         repeat_count=$((5000 / num_prompts))
+#         python benchmarks/benchmark_prefix_caching.py \
+#             --model ibm-granite/granite-4.0-h-small \
+#             --num-prompts $num_prompts \
+#             --repeat-count $repeat_count \
+#             --input-length-range $input_length:$input_length_upper \
+#             --tensor-parallel-size 4 \
+#             --dtype bfloat16 \
+#             --enable-prefix-caching > benchmarks/vllm2/dev_granite4_h_small_prefix_caching_numprompts_${num_prompts}_inputlen_${input_length}.log 2>&1
+#         done
+#   done
+
+#   # For num
+# for num_prompts in 1 5 10 25 50 100 500; do
+#     for input_length in 0 100 200 400 800 1600; do
+#         export CUDA_VISIBLE_DEVICES=0,1,2,3
+#         input_length_upper=$((input_length + 100))
+#         repeat_count=$((5000 / num_prompts))
+#         python benchmarks/benchmark_prefix_caching.py \
+#             --model ibm-granite/granite-4.0-h-small \
+#             --num-prompts $num_prompts \
+#             --repeat-count $repeat_count \
+#             --input-length-range $input_length:$input_length_upper \
+#             --tensor-parallel-size 4 \
+#             --dtype bfloat16 > benchmarks/vllm2/base_no_prefix_cache_granite4_h_small_prefix_caching_numprompts_${num_prompts}_inputlen_${input_length}.log 2>&1
+#         done
+#   done
+
+# git switch main
+# # For num
+# for num_prompts in 1 5 10 25 50 100 500; do
+#     for input_length in 0 100 200 400 800 1600; do
+#         export CUDA_VISIBLE_DEVICES=0,1,2,3
+#         input_length_upper=$((input_length + 100))
+#         repeat_count=$((5000 / num_prompts))
+#         python benchmarks/benchmark_prefix_caching.py \
+#             --model ibm-granite/granite-4.0-h-small \
+#             --num-prompts $num_prompts \
+#             --repeat-count $repeat_count \
+#             --input-length-range $input_length:$input_length_upper \
+#             --tensor-parallel-size 4 \
+#             --dtype bfloat16 \
+#             --enable-prefix-caching > benchmarks/vllm2/base_prefix_cache_granite4_h_small_prefix_caching_numprompts_${num_prompts}_inputlen_${input_length}.log 2>&1
+#         done
+#   done
--- vllm2.sh
+++ vllm2.sh
@@ -1,9 +1,53 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-python benchmarks/benchmark_prefix_caching.py \
-    --model ibm-granite/granite-4.0-h-small \
-    --num-prompts 30 \
-    --repeat-count 200 \
-    --input-length-range 0:100 \
-    --tensor-parallel-size 4 \
-    --dtype bfloat16 \
-    # --enable-prefix-caching
+
+
+for num_prompts in 1 5 10 25 50 100 500; do
+    for input_length in 0 100 200; do
+        export CUDA_VISIBLE_DEVICES=0,1,2,3
+        input_length_upper=$((input_length + 100))
+        repeat_count=$((5000 / num_prompts))
+        echo "Running for num_prompts=$num_prompts, input_length=$input_length, repeat_count=$repeat_count"
+        python benchmarks/benchmark_prefix_caching.py \
+            --model ibm-granite/granite-4.0-h-small \
+            --num-prompts $num_prompts \
+            --repeat-count $repeat_count \
+            --input-length-range $input_length:$input_length_upper \
+            --tensor-parallel-size 4 \
+            --dtype bfloat16 \
+            --enable-prefix-caching > benchmarks/vllm-prefix-cache/dev_numprompts_${num_prompts}_inputlen_${input_length}.log 2>&1
+        done
+  done
+
+  # For num
+for num_prompts in 1 5 10 25 50 100 500; do
+    for input_length in 0 100 200; do
+        export CUDA_VISIBLE_DEVICES=0,1,2,3
+        input_length_upper=$((input_length + 100))
+        repeat_count=$((5000 / num_prompts))
+        echo "Running for num_prompts=$num_prompts, input_length=$input_length, repeat_count=$repeat_count"
+        python benchmarks/benchmark_prefix_caching.py \
+            --model ibm-granite/granite-4.0-h-small \
+            --num-prompts $num_prompts \
+            --repeat-count $repeat_count \
+            --input-length-range $input_length:$input_length_upper \
+            --tensor-parallel-size 4 \
+            --dtype bfloat16 > benchmarks/vllm-prefix-cache/base_no_prefix_cache_numprompts_${num_prompts}_inputlen_${input_length}.log 2>&1
+        done
+  done
+
+git switch main
+# For num
+for num_prompts in 1 5 10 25 50 100 500; do
+    for input_length in 0 100 200; do
+        export CUDA_VISIBLE_DEVICES=0,1,2,3
+        input_length_upper=$((input_length + 100))
+        repeat_count=$((5000 / num_prompts))
+        python benchmarks/benchmark_prefix_caching.py \
+            --model ibm-granite/granite-4.0-h-small \
+            --num-prompts $num_prompts \
+            --repeat-count $repeat_count \
+            --input-length-range $input_length:$input_length_upper \
+            --tensor-parallel-size 4 \
+            --dtype bfloat16 \
+            --enable-prefix-caching > benchmarks/vllm-prefix-cache/base_prefix_cache_numprompts_${num_prompts}_inputlen_${input_length}.log 2>&1
+        done
+  done
